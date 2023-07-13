8e5624c2f6104159e6e63272cd757e149ac4b499
Dan Rashid <dan@danrashid.com>
2023-07-13 18:50:59 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+export const dom = (tag, attributesOrElement, ...elements) => {
+  const element = document.createElement(tag);
+
+  if (
+    attributesOrElement instanceof Element ||
+    typeof attributesOrElement === "string"
+  ) {
+    element.append(attributesOrElement);
+  } else if (attributesOrElement instanceof Object) {
+    Object.entries(attributesOrElement).forEach(([key, value]) => {
+      element.setAttribute(key, value);
+    });
+  }
+
+  element.append(...elements);
+
+  return element;
+};
