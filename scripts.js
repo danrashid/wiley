e4cf5ec9985fb9d37830e02deb10d857cb9ceda0
Dan Rashid <dan@danrashid.com>
2023-07-13 19:39:22 -0500

    Allow key/value pairs in attribute values

--- scripts.js
+++ scripts.js
@@ -8,7 +8,15 @@ export const el = (tag, attributesOrElement, ...elements) => {
     element.append(attributesOrElement);
   } else if (attributesOrElement instanceof Object) {
     Object.entries(attributesOrElement).forEach(([key, value]) => {
-      element.setAttribute(key, value);
+      element.setAttribute(
+        key,
+        value instanceof Object
+          ? Object.entries(value).reduce(
+              (a, [key, value]) => `${key}:${value};` + a,
+              ""
+            )
+          : value
+      );
     });
   }
 
