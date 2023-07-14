a4a09ebbaa3fbb492f1cc558f92f4675cac301c3
Dan Rashid <dan.rashid@veriforce.com>
2023-07-14 13:02:40 -0500

    Add more utilities

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+export const $ = (selectors) => {
+  const elements = document.querySelectorAll(selectors);
+
+  switch (elements.length) {
+    case 0:
+      return undefined;
+    case 1:
+      return elements[0];
+    default:
+      return elements;
+  }
+};
+
 export const el = (tag, elementOrAttributes, ...elements) => {
   const element = document.createElement(tag);
 
@@ -8,18 +21,49 @@ export const el = (tag, elementOrAttributes, ...elements) => {
     element.append(elementOrAttributes, ...elements);
   } else if (elementOrAttributes instanceof Object) {
     Object.entries(elementOrAttributes).forEach(([key, value]) => {
-      element.setAttribute(
-        key,
-        value instanceof Object
-          ? Object.entries(value).reduce(
-              (a, [key, value]) => `${key}:${value};` + a,
-              ""
-            )
-          : value
-      );
+      if (value instanceof Function) {
+        element[key] = (event) => value(event, element);
+      } else if (value instanceof Object) {
+        element.setAttribute(
+          key,
+          Object.entries(value).reduce(
+            (a, [key, value]) => a + `${key}:${value};`,
+            ""
+          )
+        );
+      } else {
+        element.setAttribute(key, value);
+      }
     });
     element.append(...elements);
   }
 
   return element;
+};
+
+export const fetch = async (resource, options = {}) => {
+  try {
+    const response = await window.fetch(resource, {
+      headers: {
+        "Content-Type": "application/json",
+        ...options.headers,
+      },
+      ...options,
+    });
+
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+
+    switch (response.headers.get("Content-Type")) {
+      case "application/json":
+        return await response.json();
+      case "text/plain":
+        return await response.text();
+      default:
+        return response;
+    }
+  } catch (e) {
+    console.error(e);
+  }
 };
